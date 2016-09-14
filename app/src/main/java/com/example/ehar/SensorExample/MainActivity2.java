1762b3745b3f69293b34177a6bd13913699ef318
Ina Maloney <Ina@Inas-MacBook-Air.local>
2016-09-14 12:09:33 -0400

    Latitude and Longitude in textview

--- app/src/main/java/com/example/ehar/SensorExample/MainActivity2.java
+++ app/src/main/java/com/example/ehar/SensorExample/MainActivity2.java
@@ -22,34 +22,43 @@ public class MainActivity2
         extends AppCompatActivity
         implements Observer {
 
-    public final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1;
+    public final int PERMISSIONS_ACCESS_FINE_LOCATION = 1;
 
     // textviews
     private TextView accel_x_view = null;
     private TextView accel_y_view = null;
     private TextView accel_z_view = null;
+    private TextView gps_lat_view = null;
+    private TextView gps_lon_view = null;
     private Observable accel;
+    private Observable gpsCo;
 
     @Override
     public void update(Observable observable, Object o) {
-        float [] values = (float []) o;
-        accel_x_view.setText(Float.toString(values[0]));
-        accel_y_view.setText(Float.toString(values[1]));
-        accel_z_view.setText(Float.toString(values[2]));
+        if (observable == gpsCo) {
+            double [] coordinates = (double []) o;
+            gps_lat_view.setText(Double.toString(coordinates[0]));
+            gps_lon_view.setText(Double.toString(coordinates[1]));
+
+        }
+        else {
+            float [] values = (float []) o;
+            accel_x_view.setText(Float.toString(values[0]));
+            accel_y_view.setText(Float.toString(values[1]));
+            accel_z_view.setText(Float.toString(values[2]));
+        }
     }
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int permissionCheck = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            // Ask the user for permision
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS},
-                    PERMISSIONS_ACCESS_COARSE_LOCATION);
-        }
+        gps_lat_view = (TextView) findViewById(R.id.gps_lat);
+        gps_lon_view = (TextView) findViewById(R.id.gps_lon);
+        this.getPermissions();
+        this.gpsCo = new gpsCooridinates(this);
+        this.gpsCo.addObserver(this);
 
 
         accel_x_view = (TextView) findViewById(R.id.accel_x);
@@ -59,17 +68,26 @@ public class MainActivity2
         this.accel.addObserver(this);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_ACCESS_COARSE_LOCATION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // All good!
-                } else {
-                    Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
-                }
+    public void getPermissions() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
-                break;
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_ACCESS_FINE_LOCATION);
+            }
         }
     }
 
--- app/src/main/java/com/example/ehar/SensorExample/gpsCooridinates.java
+++ app/src/main/java/com/example/ehar/SensorExample/gpsCooridinates.java
@@ -1,8 +1,18 @@
 package com.example.ehar.SensorExample;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import java.util.Observable;
 
@@ -10,17 +20,48 @@ import java.util.Observable;
  * Created by Ina on 9/12/16.
  */
 public class gpsCooridinates
-        extends Observable
-        implements SensorEventListener {
+        extends Observable {
 
+    LocationManager locationManager;
+    LocationListener locationListener;
+//    Location location;
+    double lat = 0;
+    double lon = 0;
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
+    public gpsCooridinates(Activity act) {
+        locationManager = (LocationManager) act.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            // Called when a new location is found by the network location provider.
+            @Override
+            public void onLocationChanged(Location location) {
+                System.out.println("changed");
+                lat = location.getLatitude();
+                lon = location.getLongitude();
+                double [] coordinates = {lat, lon};
+                System.out.println(coordinates.toString());
+                setChanged();
+                notifyObservers(coordinates);
 
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        if (ContextCompat.checkSelfPermission(act, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
 }
